59bcf4aeb29633b75d9963e2bce9d615e36658d6
Can Okulmuş <can.okulmus.can@gmail.com>
2023-04-01 14:45:36 +0300

    extra colors for theme

--- src/assets/theme/base/colors.tsx
+++ src/assets/theme/base/colors.tsx
@@ -50,7 +50,57 @@ const colors = {
   sidenavLink: {
     active: '#1a1f37',
     activeIcon: '#DB631C', 
-  }
+  },
+
+  badgeColors: {
+    primary: {
+      basic: "#805ad5",
+      background: "#f883dd",
+      text: "#a3017e",
+    },
+
+    secondary: {
+      basic: "#5974a2",
+      background: "#e4e8ed",
+      text: "#5974a2",
+    },
+
+    info: {
+      basic: "#4299e1",
+      background: "#abe9f7",
+      text: "#08a1c4",
+    },
+
+    success: {
+      basic: "#01b574",
+      background: "#c9fbd5",
+      text: "#01b574",
+    },
+
+    warning: {
+      basic: "#ffb547",
+      background: "#fef5d3",
+      text: "#fbc400",
+    },
+
+    error: {
+      basic: "#e31a1a",
+      background: "#fc9797",
+      text: "#bd0000",
+    },
+
+    light: {
+      basic: "#ffffff",
+      background: "#ffffff",
+      text: "#c7d3de",
+    },
+
+    dark: {
+      basic: "#1E244B",
+      background: "#1E244B",
+      text: "#fff",
+    },
+  },
 };
 
 export default colors;
